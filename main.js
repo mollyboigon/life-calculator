b77855c930e66fadf82cab5207dd1680d2fe4dcb
Molly Boigon <molly.boigon79@journalism.cuny.edu>
2022-04-12 13:30:11 -0400

    did javascript

--- main.js
+++ main.js
@@ -24,48 +24,33 @@ inputs.forEach(function (input) { /* input is gonna be the fruit*/
         const qty = parseFloat(e.target.value);
         const this_row = this_input.closest(".row") //closest looks at element first then element's parent if it doesn't match the selector, then grandparent, so forth, till it gets a match
         //will keep working up until it finds div class row
-        const amazon = this_row.querySelector(".amazon"); //will find all descendants of this row with the class amazon on them;
-            const amazon_span = amazon.querySelector("span"); // amazon span = go inside of it and find that span that's inside of it
-            const amazon_price = parseFloat(amazon.dataset.price);  //amazon.dataset.price captures dataset as a string, parselfoat turns it into float 
-                const amazon_cost = qty * amazon_price;
-                amazon_span.innerHTML = round_number(amazon_cost); 
-                amazon.classList.add("active");//telling javascript to update node so that content is whatever you put on the other side of the equals sign
+        const sleep = this_row.querySelector(".sleep"); //will find all descendants of this row with the class amazon on them;
+            const sleep_span = amazon.querySelector("span"); // amazon span = go inside of it and find that span that's inside of it
+            const sleep_rate = parseFloat(sleep.dataset.rate);  //amazon.dataset.price captures dataset as a string, parselfoat turns it into float 
+                const sleep_years = qty * sleep_rate;
+                sleep_span.innerHTML = round_number(sleep_years); 
+                sleep.classList.add("active");//telling javascript to update node so that content is whatever you put on the other side of the equals sign
 
-        const freshdirect = this_row.querySelector(".freshdirect");
-            const freshdirect_span = freshdirect.querySelector("span");
-            const freshdirect_price = parseFloat(freshdirect.dataset.price);
-            const freshdirect_cost = qty * freshdirect_price;
-                freshdirect_span.innerHTML = round_number(freshdirect_cost);
-                freshdirect.classList.add("active");
+        const work = this_row.querySelector(".work");
+            const work_span = work.querySelector("span");
+            const work_rate = parseFloat(work.dataset.rate);
+            const work_years = qty * work_rate;
+                work_span.innerHTML = round_number(work_years);
+                work.classList.add("active");
 
-        const peapod = this_row.querySelector(".peapod");
-            const peapod_span = peapod.querySelector("span");
-            const peapod_price = parseFloat(peapod.dataset.price);
-            const peapod_cost = qty * peapod_price;
-                peapod_span.innerHTML = round_number(peapod_cost);
-                peapod.classList.add("active");
+        const tv = this_row.querySelector(".tv");
+            const tv_span = tv.querySelector("span");
+            const tv_rate = parseFloat(tv.dataset.rate);
+            const tv_years = qty * tv_rate;
+                tv_span.innerHTML = round_number(tv_years);
+                tv.classList.add("active");
+
+        const eating = this_row.querySelector(".eating");
+            const eating_span = eating.querySelector("span");
+            const eating_rate = parseFloat(eating.dataset.rate);
+            const eating_years = qty * eating_rate;
+                    eating_span.innerHTML = round_number(eating_years);
+                    eating.classList.add("active");
         
-        let cheap = false;
-
-        if(amazon_cost < freshdirect_cost && amazon_cost < peapod_cost) { //conditional
-            cheap = amazon;
-        }
-
-        if(freshdirect_cost < amazon_cost && freshdirect_cost < peapod_cost) { //conditional
-            cheap = freshdirect;
-        }
-
-        if(peapod_cost < amazon_cost && peapod_cost < freshdirect_cost) {
-            cheap = peapod;
-        }
-
-        const current_cheap = this_row.querySelector(".cheap");
-
-        if (current_cheap) {
-            current_cheap.classList.remove("cheap");
-        }
-        if (cheap) {
-        cheap.classList.add("cheap");
-        }
         });
     });
